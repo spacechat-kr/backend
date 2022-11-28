a987640d6d9e2dfca09add8513c74d5f5a5383ee
kha-github <rlahyuna1234@gmail.com>
2022-11-29 01:23:09 +0900

    init project

---
+++ src/main/java/team/spacechat/spacechatapi/SpacechatApiApplication.java
@@ -0,0 +1,13 @@
+package team.spacechat.spacechatapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpacechatApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpacechatApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/team/spacechat/spacechatapi/SpacechatApiApplicationTests.java
@@ -0,0 +1,13 @@
+package team.spacechat.spacechatapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpacechatApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
